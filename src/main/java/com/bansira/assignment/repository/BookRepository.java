31abda4e30953b3cbd3da265e1a41cef10718086
Ashutosh Mishra <mishrashutosh1998@gmail.com>
2024-07-11 01:49:53 +0530

    Other methods added to service

--- src/main/java/com/bansira/assignment/repository/BookRepository.java
+++ src/main/java/com/bansira/assignment/repository/BookRepository.java
@@ -4,6 +4,12 @@ import com.bansira.assignment.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByISBN(String ISBN);
+    List<Book> findByTitleIgnoreCase(String title);
+    List<Book> findByAuthorIgnoreCase(String author);
+    List<Book> findByAvailability(boolean availability);
 }
--- src/main/java/com/bansira/assignment/service/LibraryService.java
+++ src/main/java/com/bansira/assignment/service/LibraryService.java
@@ -5,6 +5,8 @@ import com.bansira.assignment.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LibraryService {
 
@@ -13,5 +15,28 @@ public class LibraryService {
 
     public Book addBook(Book book) {
         return bookRepository.save(book);
+    }
+
+    public void removeBook(String ISBN) {
+        Book book = bookRepository.findByISBN(ISBN);
+        if (book != null) {
+            bookRepository.delete(book);
+        }
+    }
+
+    public List<Book> findBookByTitle(String title) {
+        return bookRepository.findByTitleIgnoreCase(title);
+    }
+
+    public List<Book> findBookByAuthor(String author) {
+        return bookRepository.findByAuthorIgnoreCase(author);
+    }
+
+    public List<Book> listAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public List<Book> listAvailableBooks() {
+        return bookRepository.findByAvailability(true);
     }
 }
